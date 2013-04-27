1030b64014410516fd102871292a43554fb1daee
odoutriaux <doutriaux.olivier@gmail.com>
2013-04-27 19:07:19 +0200

    Modification 27/04/13

--- project/app/models/product.rb
+++ project/app/models/product.rb
@@ -1,4 +1,25 @@
 class Product < ActiveRecord::Base
-	 belongs_to :category
-	 default_scope :order => 'title'
+	belongs_to :category
+
+	validates :title, :description, :image_url, :price, :category_id, :presence => true
+	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+	validates :title, :uniqueness => true
+	validates :image_url, format: { with: %r{\.gif|jpg|png}i, message: 'must be a url for gif, jpg, or png image.' }
+
+	
+	default_scope :order => 'title'
+
+	default_scope :order => 'title'
+
+	has_many :line_items
+	before_destroy :ensure_not_referenced_by_any_line_item
+	# ensure that there are no line items referencing this product
+	def ensure_not_referenced_by_any_line_item
+		if line_items.count.zero?
+			return true
+		else
+			errors[:base] << "Line Items present"
+			return false
+		end
+	end
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,54 +1,34 @@
 Project::Application.routes.draw do
+  resources :orders
+
+  get "admin/index"
+  get "sessions/new"
+  get "sessions/create"
+  get "sessions/destroy"
+  resources :users
+
+  resources :line_items
+
+  resources :carts
+
+  get "store/index"
   get "home/index"
-  resources :products
+  get 'admin' => 'admin#index'
+  get 'store' => 'store#index'
 
   resources :categories
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  controller :sessions do
+    get 'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
+  resources :products do
+    get :who_bought, :on => :member
+  end
 
   # You can have the root of your site routed with "root"
-   root to: 'home#index', :as => 'home'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    root to: 'home#index', :as => 'home'
+ 
 end
